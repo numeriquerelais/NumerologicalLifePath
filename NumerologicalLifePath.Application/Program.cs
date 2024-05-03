0df0d2d11b30e679941ecd95dae521c4357ea97f
antrax <antrax2013@hotmail.com>
2024-05-03 09:02:14 +0200

    Add dotnet-stryker mutation tester
    Add commandLine Application

---
+++ NumerologicalLifePath.Application/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.CommandLine;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var fileOption = new Option<FileInfo?>(
+           name: "--files",
+           description: "The file to read and display on the console.");
+
+var rootCommand = new RootCommand("Sample app for System.CommandLine");
+rootCommand.AddOption(fileOption);
+
+rootCommand.SetHandler((file) => { ReadFile(file!);}, fileOption);
+
+return await rootCommand.InvokeAsync(args);
+
+static void ReadFile(FileInfo file)
+{
+    Console.WriteLine($"I'm reading file {file.FullName}");
+}

8797584845ac7ddde897fa47b7fde0232ffd1560
numeriquerelais <75170213+numeriquerelais@users.noreply.github.com>
2024-05-13 23:43:52 +0200

    fix(Sonar) : apply sonar suggestions

---
+++ NumerologicalLifePath.Application/ArgumentResultClassExtension.cs
@@ -0,0 +1,14 @@
+﻿namespace NumerologicalLifePath.Application;
+
+public static class ArgumentResultClassExtension
+{
+    public static DateOnly ConvertArgToDateOnly(this System.CommandLine.Parsing.ArgumentResult result)
+    {
+        string? input = result.Tokens[0]?.Value.Replace("-", "/").Replace(".", "/");
+
+        if (DateOnly.TryParseExact(input, ["dd/MM/yyyy", "MM/dd/yyyy", "yyyy/MM/dd"], out DateOnly date))
+            return date;
+        throw new CannotParseArugmentException($"Cannot parse argument '{input}' as expected type 'System.DateOnly'.");
+        
+    }
+}

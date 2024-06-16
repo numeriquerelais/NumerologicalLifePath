1dbf5d58323c50e28d04c65613b1e1dde95c3608
antrax <antrax2013@hotmail.com>
2024-06-16 07:58:31 +0200

    fix typo

--- NumerologicalLifePath.Application/CliCommands/InclusionGridCliCommand.cs
+++ NumerologicalLifePath.Application/CliCommands/InclusionGridCliCommand.cs
@@ -37,7 +37,7 @@ public sealed class InclusionGridCliCommand : TwoOptionsCliCommandWrapper<string
                         } 
                     }
 
-                    Console.WriteLine($"The inclusion grid of {string.Join(" ", firstNames)} {string.Join(" ", lastNames)} are : \r\n{infos}");
+                    Console.WriteLine($"The inclusion grid of {string.Join(" ", firstNames)} {string.Join(" ", lastNames)} is : \r\n{infos}");
                 },
                 ["-f", "-l"]
         ))
